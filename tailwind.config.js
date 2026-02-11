ba342b6c12caa3cf2f0bd2c5714a8ef6e4fc4923
Andrew Birch <ab8218-cp@yahoo.com>
2026-02-11 15:35:30 +0000

    FIX: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// @type {import('tailwindcss').Config}
+// changed modules to module
+module.export = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
